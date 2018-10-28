576eb75b1fac495a2a0958dcfb04ed3f7ed90b30
Labbi-Ahmed <labbi5689@gmail.com>
2018-10-29 00:39:38 +0600

    two button are connected with two layout

--- app/src/main/java/com/example/labib/mywordbanks/MainActivity.java
+++ app/src/main/java/com/example/labib/mywordbanks/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.labib.mywordbanks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button collected_word , save_word;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        collected_word = (Button) findViewById(R.id.new_word);
+        save_word = (Button) findViewById(R.id.my_collection);
+
+        save_word.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.updateword);
+            }
+        });
+
+        collected_word.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.collected_words);
+            }
+        });
     }
 
 
